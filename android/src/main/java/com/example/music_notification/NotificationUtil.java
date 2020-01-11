f68e4a28eb4213667f4931a95eb894e9b90471f8
xuzhongpeng <1452754335@qq.com>
2020-01-11 14:02:01 +0800

    feat:添加监听

--- android/src/main/java/com/example/music_notification/NotificationUtil.java
+++ android/src/main/java/com/example/music_notification/NotificationUtil.java
@@ -1,6 +1,5 @@
 package com.example.music_notification;
 
-
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -33,8 +32,7 @@ import io.flutter.plugin.common.FlutterException;
 import io.flutter.plugin.common.MethodCall;
 
 /**
- * 显示通知栏工具类
- * Created by jsshou on 2020/01/09.
+ * 显示通知栏工具类 Created by jsshou on 2020/01/09.
  */
 
 public class NotificationUtil {
@@ -42,14 +40,16 @@ public class NotificationUtil {
         byte[] imgUrl = params.argument("imgUrl");
         String musicName = params.argument("musicName").toString();
         String singer = params.argument("singer").toString();
-        //big
+        Bool isPlaying = params.argument("isPlaying");
+        // big
         Intent button1I = new Intent(MyBroadcastReceiver.ACTION_1);
         PendingIntent button1PI = PendingIntent.getBroadcast(context, 0, button1I, 0);
         Intent button2I = new Intent(MyBroadcastReceiver.ACTION_2);
         PendingIntent button2PI = PendingIntent.getBroadcast(context, 0, button2I, 0);
         Intent button3I = new Intent(MyBroadcastReceiver.ACTION_3);
         PendingIntent button3PI = PendingIntent.getBroadcast(context, 0, button3I, 0);
-        RemoteViews remoteViews = new RemoteViews(context.getApplicationContext().getPackageName(), R.layout.notification);
+        RemoteViews remoteViews = new RemoteViews(context.getApplicationContext().getPackageName(),
+                R.layout.notification);
         remoteViews.setOnClickPendingIntent(R.id.last, button1PI);
         remoteViews.setOnClickPendingIntent(R.id.next, button2PI);
         remoteViews.setOnClickPendingIntent(R.id.play, button3PI);
@@ -59,8 +59,9 @@ public class NotificationUtil {
         remoteViews.setTextViewText(R.id.musicName, musicName);
         remoteViews.setTextViewText(R.id.singer, singer);
 
-        //small
-        RemoteViews sremoteViews = new RemoteViews(context.getApplicationContext().getPackageName(), R.layout.notificationsmal);
+        // small
+        RemoteViews sremoteViews = new RemoteViews(context.getApplicationContext().getPackageName(),
+                R.layout.notificationsmal);
         Intent sbutton2I = new Intent(MyBroadcastReceiver.ACTION_2);
         PendingIntent sbutton2PI = PendingIntent.getBroadcast(context, 0, sbutton2I, 0);
         sremoteViews.setOnClickPendingIntent(R.id.next1, sbutton2PI);
@@ -73,33 +74,29 @@ public class NotificationUtil {
         PendingIntent sbutton3PI = PendingIntent.getBroadcast(context, 0, sbutton3I, 0);
         sremoteViews.setOnClickPendingIntent(R.id.play1, sbutton3PI);
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        //Create channel in which to send push notifications
-        String CHANNEL_ID = "my_channel_01";
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        // Create channel in which to send push notifications
+        String CHANNEL_ID = "musicNotification";
         // only use NotificationChannel when Api Level >= 26
         if (android.os.Build.VERSION.SDK_INT >= 26) {
-            CharSequence name = "my_channel";
-            String Description = "This is my channel";
+            CharSequence name = "musicNotification";
+            String Description = "musicNotification";
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
             mChannel.setDescription(Description);
             mChannel.enableLights(true);
             mChannel.setLightColor(Color.RED);
             mChannel.enableVibration(true);
-            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setVibrationPattern(new long[] { 100, 200, 300, 400, 500, 400, 300, 200, 400 });
             mChannel.setShowBadge(false);
             notificationManager.createNotificationChannel(mChannel);
         }
 
-//Send push notification
+        // Send push notification
         Notification notify = new NotificationCompat.Builder(context.getApplicationContext(), CHANNEL_ID)
-                .setContentTitle("Listener available on pSquared!")
-                .setContentText("You can now talk about your day in a pSquared chatbox with a Listener")
-                .setSmallIcon(R.raw.play)
-                .setCustomBigContentView(remoteViews)
-                .setContent(sremoteViews)
-                .setAutoCancel(false)
-                .setOngoing(true)
+                .setContentTitle("musicNotification").setContentText("musicNotification").setSmallIcon(R.raw.play)
+                .setCustomBigContentView(remoteViews).setContent(sremoteViews).setAutoCancel(false).setOngoing(true)
                 .build();
 
         notify.flags |= Notification.FLAG_AUTO_CANCEL;
@@ -113,17 +110,13 @@ public class NotificationUtil {
      * @return
      */
 
-
     static DisplayImageOptions options = new DisplayImageOptions.Builder()
-//                .cacheInMemory(true) //设置下载的图片是否缓存在内存中
-//                .cacheOnDisc(true)//设置下载的图片是否缓存在SD卡中
-//                .showImageOnLoading(R.drawable.zhanwei_avater)
-//                .showImageOnFail(R.drawable.zhanwei_avater)
-            .bitmapConfig(Bitmap.Config.RGB_565)
-            .considerExifParams(false)//是否考虑JPEG图像EXIF参数（旋转，翻转）
+            // .cacheInMemory(true) //设置下载的图片是否缓存在内存中
+            // .cacheOnDisc(true)//设置下载的图片是否缓存在SD卡中
+            // .showImageOnLoading(R.drawable.zhanwei_avater)
+            // .showImageOnFail(R.drawable.zhanwei_avater)
+            .bitmapConfig(Bitmap.Config.RGB_565).considerExifParams(false)// 是否考虑JPEG图像EXIF参数（旋转，翻转）
             .resetViewBeforeLoading(false)// 设置图片在下载前是否重置，复位
             .build();
 
-
 }
-
--- lib/music_notification.dart
+++ lib/music_notification.dart
@@ -17,7 +17,7 @@ class MusicNotification {
     try {
       _doHandlePlatformCall(call);
     } catch (ex) {
-      print('Unexpected error: $ex');
+      print('MusicNotification Unexpected error : $ex');
     }
   }
 
@@ -25,23 +25,62 @@ class MusicNotification {
     final Map<dynamic, dynamic> callArgs = call.arguments as Map;
     print('_platformCallHandler call ${call.method} $callArgs');
     switch (call.method) {
+      case "playing":
+        {
+          if (callArgs['isPlay'] == true) {
+            _stateChange.add(PlayerState.Playing);
+          } else {
+            _stateChange.add(PlayerState.Stopping);
+          }
+          break;
+        }
+      case "next":
+        {
+          _nextController.add(null);
+          break;
+        }
+      case "last":
+        {
+          _lastController.add(null);
+          break;
+        }
     }
   }
 
-  final StreamController<Duration> stateChange =
-      StreamController<Duration>.broadcast();
+  static final StreamController<PlayerState> _stateChange =
+      StreamController<PlayerState>.broadcast();
+  static final StreamController<void> _nextController =
+      StreamController<void>.broadcast();
+  static final StreamController<void> _lastController =
+      StreamController<void>.broadcast();
+
+  static Stream<PlayerState> get stateChangeStream => _stateChange.stream;
+  static Stream<void> get nextStream => _nextController.stream;
+  static Stream<void> get lastStream => _lastController.stream;
+
+  Future<void> dispose() async {
+    List<Future> futures = [];
+    if (!_stateChange.isClosed) futures.add(_stateChange.close());
+    if (!_nextController.isClosed) futures.add(_nextController.close());
+    if (!_lastController.isClosed) futures.add(_lastController.close());
+    await Future.wait(futures);
+  }
 }
+
+enum PlayerState { Playing, Stopping }
 
 class NotifiParams {
   Uint8List imgUrl;
   String musicName;
   String singer;
-  NotifiParams({this.imgUrl, this.musicName, this.singer});
+  bool isPlaying;
+  NotifiParams({this.imgUrl, this.musicName, this.singer, this.isPlaying});
   Map<String, dynamic> toJson() {
     return {
       "imgUrl": this.imgUrl,
       "musicName": this.musicName,
-      "singer": this.singer
+      "singer": this.singer,
+      "isPlaying": this.isPlaying
     };
   }
 }
